9655e67dde74785e4fcfdd640e30c5789fd0efc0
Vikas Kumar <44707673+vkn84527@users.noreply.github.com>
2021-07-05 00:01:57 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-
-var mydb_conn = require('./service/db_connection')
-
-app.listen(port, () => {
-    console.log("Server is Running on port 3000")
-})
