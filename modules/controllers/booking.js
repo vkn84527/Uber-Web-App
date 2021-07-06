f243ade12c33339ab3d5f61c605bdd6c4bdca108
Vikas Kumar <44707673+vkn84527@users.noreply.github.com>
2021-07-07 00:39:57 +0530

    Add files via upload

---
+++ modules/controllers/booking.js
@@ -0,0 +1,15 @@
+
+// function booking_ride(req, res) {
+//   var customer_email = req.body.customer_email;
+//   var customer_password = req.body.customer_password;
+// console.log(customer_email)
+  
+// }
+
+// booking_ride()
+
+module.exports.booking_ride = function (req, res) {
+
+   
+}
+    
