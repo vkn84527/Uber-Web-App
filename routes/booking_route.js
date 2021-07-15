08f5e8244dc5609275255e23394e75c6c8d6231a
vikas kumar <vikas.kumar1@jungleworks.com>
2021-07-15 11:58:35 +0530

    cancel booking by customer and driver

--- routes/booking_route.js
+++ routes/booking_route.js
@@ -6,7 +6,8 @@ module.exports = app => {
 
     app.post('/find_driver', checkAuth.customer, booking.find_drivers)
     app.post('/check_bookings', checkAuth.customer, booking.check_bookings)
-    app.post('/cancel_booking', checkAuth.customer, booking.cancel_booking)
+    app.post('/customer/cancel_booking', checkAuth.customer , booking.cancel_booking)
+    app.post('/driver/cancel_booking', checkAuth.driver , booking.cancel_booking)
     app.post('/completed_booking', checkAuth.driver, booking.completed_booking)
     app.post('/book_rides', checkAuth.customer, booking.ride_booking)
 
